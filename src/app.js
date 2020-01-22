e777012818d4251c90a831493caf6b32ac4c67ff
Jenny Ardrey <jenny@ardrey.co.uk>
2020-01-22 18:13:05 +0000

    refactored routes

--- src/app.js
+++ src/app.js
@@ -1,103 +1,15 @@
 const express = require('express');
-const stringsRouter = require('./routes/strings');
-const {
-  sayHello,
-  uppercase,
-  lowercase,
-  firstCharacter,
-  firstCharacters,
-} = require('./lib/strings');
-const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
+const stringsRouter = require('./routes/strings.js');
+const numbersRouter = require('./routes/numbers.js');
+const booleansRouter = require('./routes/booleans');
+const arraysRouter = require('./routes/arrays');
 
 const app = express();
 
 app.use(express.json());
 app.use('/strings', stringsRouter);
-
-app.get(`/strings/hello/:string`, (req, res) => {
-  res.json({ result: sayHello(req.params.string) }).sendStatus(200);
-});
-app.get(`/strings/upper/:string`, (req, res) => {
-  res.json({ result: uppercase(req.params.string) }).sendStatus(200);
-});
-app.get(`/strings/lower/:string`, (req, res) => {
-  res.json({ result: lowercase(req.params.string) }).sendStatus(200);
-});
-app.get(`/strings/first-character/:string`, (req, res) => {
-  res.json({ result: firstCharacter(req.params.string) }).sendStatus(200);
-});
-app.get(`/strings/first-characters/:string`, (req, res) => {
-  res.json({ result: firstCharacters(req.params.string, req.query.length) }).sendStatus(200);
-});
-app.get(`/numbers/add/:number1/and/:number2`, (req, res) => {
-  const num1 = Number(req.params.number1);
-  const num2 = Number(req.params.number2);
-  const added = add(num1, num2);
-  return Number.isNaN(num1) || Number.isNaN(num2)
-    ? res.sendStatus(400)
-    : res.status(200).json({ result: added });
-});
-app.get(`/numbers/subtract/:number1/from/:number2`, (req, res) => {
-  const num1 = req.params.number1;
-  const num2 = req.params.number2;
-  const subtracted = subtract(num2, num1);
-  if (Number.isNaN(Number(num1)) || Number.isNaN(Number(num2))) {
-    return res.status(400).send({ error: 'Parameters must be valid numbers.' });
-  }
-  res.status(200).send({ result: subtracted });
-});
-
-app.post(`/numbers/multiply`, (req, res) => {
-  const { a, b } = req.body;
-  if (!a || !b) {
-    return res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
-  }
-  if (Number.isNaN(Number(a)) || Number.isNaN(Number(b))) {
-    return res.status(400).send({ error: 'Parameters "a" and "b" must be valid numbers.' });
-  }
-  res.status(200).json({ result: multiply(req.body.a, req.body.b) });
-});
-
-app.post(`/numbers/divide`, (req, res) => {
-  const { a, b } = req.body;
-  // console.log(a);
-
-  if (b === 0) {
-    return res.status(400).send({ error: 'Unable to divide by 0.' });
-  }
-  // console.log(a);
-
-  if (!a || !b) {
-    return res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
-  }
-  // console.log(a);
-
-  if (Number.isNaN(Number(a)) || Number.isNaN(Number(b))) {
-    return res.status(400).send({ error: 'Parameters "a" and "b" must be valid numbers.' });
-  }
-  if (a === 0) {
-    return res.status(200).send({ result: 0 });
-  }
-  res.status(200).json({ result: divide(a, b) });
-});
-
-app.get(`/numbers/remainder`, (req, res) => {
-  const { a, b } = req.body;
-
-  if (b === 0) {
-    return res.status(400).send({ error: 'Unable to divide by 0.' });
-  }
-
-  if (!a || !b) {
-    return res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
-  }
-  if (Number.isNaN(Number(a)) || Number.isNaN(Number(b))) {
-    return res.status(400).send({ error: 'Parameters must be valid numbers.' });
-  }
-  if (a === 0) {
-    return res.status(200).send({ result: 0 });
-  }
-  res.status(200).send({ result: remainder(a, b) });
-});
+app.use('/numbers', numbersRouter);
+app.use('/booleans', booleansRouter);
+app.use('/arrays', arraysRouter);
 
 module.exports = app;
--- src/routes/strings.js
+++ src/routes/strings.js
@@ -1,5 +1,28 @@
 const express = require('express');
+const {
+  sayHello,
+  uppercase,
+  lowercase,
+  firstCharacter,
+  firstCharacters,
+} = require('../lib/strings');
 
 const router = express.Router();
+
+router.get(`/hello/:string`, (req, res) => {
+  res.json({ result: sayHello(req.params.string) }).sendStatus(200);
+});
+router.get(`/upper/:string`, (req, res) => {
+  res.json({ result: uppercase(req.params.string) }).sendStatus(200);
+});
+router.get(`/lower/:string`, (req, res) => {
+  res.json({ result: lowercase(req.params.string) }).sendStatus(200);
+});
+router.get(`/first-character/:string`, (req, res) => {
+  res.json({ result: firstCharacter(req.params.string) }).sendStatus(200);
+});
+router.get(`/first-characters/:string`, (req, res) => {
+  res.json({ result: firstCharacters(req.params.string, req.query.length) }).sendStatus(200);
+});
 
 module.exports = router;
